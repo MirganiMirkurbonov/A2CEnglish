12a796a5b803d19938a098f0cfa0fe20e207f33a
MirganiMirkurbonov@Gmail.com <MirganiMirkurbonov@Gmail.com>
2025-01-01 11:58:09 +0500

    [Feature] caching added

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using API.Extensions;
 using Database.Helpers;
+using Microsoft.Extensions.Caching.Hybrid;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,10 @@ builder.Services.AddControllers();
 builder.Services
     .AddEndpointsApiExplorer()
     .AddSwaggerGen();
+
+#pragma warning disable EXTEXP0018
+builder.Services.AddHybridCache(); 
+#pragma warning restore EXTEXP0018
 
 builder.Services.AddHandlerForBadRequest();
 
--- Database/DataAccess/PermissionDataAccess.cs
+++ Database/DataAccess/PermissionDataAccess.cs
@@ -1,8 +1,29 @@
 ﻿using Database.Interfaces;
+using Database.Tables;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Hybrid;
 
 namespace Database.DataAccess;
 
-internal class PermissionDataAccess : IPermissionDataAccess
+internal class PermissionDataAccess(HybridCache hybridCache, EntityContext entityContext) : IPermissionDataAccess
 {
-    
+    public async Task<IReadOnlyCollection<Permission>> GetRolePermissions(string roleKey)
+    {
+        var cacheKey = $"permissions:{roleKey}";
+        return await hybridCache.GetOrCreateAsync(cacheKey, async _ => await GetPermissions(roleKey),
+            new HybridCacheEntryOptions
+            {
+                Expiration = TimeSpan.FromMinutes(20)
+            });
+    }
+
+    private async Task<List<Permission>> GetPermissions(string roleKey)
+    {
+        var role = await entityContext.Roles.FirstAsync(x => x.Keyword == roleKey);
+        var rolePermission = await entityContext.RolePermissions
+            .Where(x => x.RoleId == role.Id)
+            .Select(x => x.Permission)
+            .ToListAsync();
+        return rolePermission;
+    }
 }
